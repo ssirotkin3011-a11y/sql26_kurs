aa2cbfb10d4a205f166d6b03963fcd5b0dff056e
ssirotkin3011-a11y <ssirotkin3011@gmail.com>
2026-03-25 18:01:39 +0300

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO words (en_word, ru_word) VALUES
+('I', 'Я'),
+('You', 'Ты'),
+('He', 'Он'),
+('She', 'Она'),
+('We', 'Мы'),
+('They', 'Они'),
+('Red', 'Красный'),
+('Green', 'Зелёный'),
+('Blue', 'Синий'),
+('Black', 'Чёрный');
